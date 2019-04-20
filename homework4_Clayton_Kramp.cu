54d17efa1c41e28b2f2d3a307c8de627ed27d392
krampcg23 <krampcg23@gmail.com>
2019-04-20 12:03:46 -0600

    Finished Serial Version

--- homework4_Clayton_Kramp.cu
+++ homework4_Clayton_Kramp.cu
@@ -1,30 +1,27 @@
 #include <iostream>
+#include <cstdlib>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        cerr << "Incorrect input style, please do ./homework4 N" << endl;
+        std::cerr << "Incorrect input style, please do ./homework4 N" << std::endl;
         return 2;
     }
 
-    int N = argv[1];
-    int* arr = new int[N];
+    int N = atoi(argv[1]);
+    long int* arr = new long int[N];
 
     for (int i = 0; i < N; i++) {
         arr[i] = rand() % 1000 + 1;
     }
 
-    int* A_cpu = new int[N];
+    long int* A_cpu = new long int[N];
     A_cpu[0] = 0;
     for (int i = 1; i < N; i++) {
-        for (int j = 0; j <= i; j++) {
+        for (int j = 0; j < i; j++) {
             A_cpu[i] += arr[j];
         }
     }
-
-    for (int i = 0; i < N; i++) {
-        printf("%i,  %i", A_cpu[i], arr[i]);
-    }
-
     return 0;
 }
 
